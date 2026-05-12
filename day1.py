dd01669da696ec6053ff61f384dc24cafe593a3e
Purvi-Sharma <mailtopurvisharma@gmail.com>
2026-05-13 01:27:15 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,29 @@
+print("hello world")
+print("hello's world")
+print('hello world')
+#print('hello's world') shows error
+print('hello"s world')
+print("hello's world")
+"hello"
+'hello'
+'''heloo
+world'''
+print('''hello
+world''')
+import calendar
+#calendar is module (collection of libraies)
+print(calendar.calendar(2026))
+print(calendar.month(2026,4))
+print("enter a no.")
+a=int(input())
+print(calendar.calendar(a))
+print("enter two no.'s")
+b=int(input())
+c=int(input())
+print(calendar.month(b,c))
+import random
+print(random.randint(50,4000))
+import cmath
+print(cmath.sqrt(-5))
+print(cmath.pi)
+print(cmath.e)
