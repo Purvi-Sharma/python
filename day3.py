585f26dbb3980f9da792bc20929255e1916e5a67
Purvi-Sharma <mailtopurvisharma@gmail.com>
2026-06-27 23:30:47 +0530

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,27 @@
+#python -dynamically typed language
+a=20
+b=30
+c=a+b
+print(c)
+print(type(c))
+d=3.0
+e=4.5
+f=d+e
+print(f)
+print(type(f))
+g=True
+print(g)
+print(type(g))
+h="True"
+print(h)
+print(type(h))
+#error
+#int i=78
+#print(i)
+
+#python -high level programming language
+m=0b1010#binary no.
+print(m)#output always in high level language =10
+x=0xAB37#hexdecimal mo.
+print(x)
+ 
